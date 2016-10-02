5ab1fb85274b0795479ab3083cfbef6c514c3c21
GuillaumeGas <guillaume@gas-ntic.fr>
2016-10-02 13:40:04 +0200

    Fix transaction create error

--- account.py
+++ account.py
@@ -5,7 +5,8 @@ from openerp import models, fields
 class Account (models.Model):
     _name = 'account.account'
 
-    name = fields.Char(string="Compte", required=True)
-    value = fields.Float(string="Solde", default=0.0)
+    name = fields.Char(string="Name", required=True)
+    value = fields.Float(string="Value", default=0.0)
 
-    transaction_ids = fields.One2many('account.transaction', 'account_id', ondelete='cascade')
+    transaction_ids = fields.One2many(
+        'account.transaction', 'account_id', string="Transactions")
--- transaction.py
+++ transaction.py
@@ -1,28 +1,41 @@
 # -*- coding: utf-8 -*-
 from openerp import models, fields, api
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class Transaction (models.Model):
     _name = 'account.transaction'
 
-    name = fields.Char(string="Nom", required=True)
-    value = fields.Float(string="Valeur", required=True)
+    name = fields.Char(string="Name", required=True)
+    value = fields.Float(string="Value", required=True)
     date = fields.Date(string="Date", required=True, default=fields.Date.today())
     validated = fields.Boolean(string="Validated", default=True)
 
-    account_id = fields.Many2one('account.account', string="Compte", required=True)
+    account_id = fields.Many2one('account.account', string="Account",
+                                 required=True, ondelete='cascade')
 
     @api.model
     def create(self, values):
-        if (self.validated):
-            self.account_id.value -= self.value
+        if (values['validated']):
+            account = self.env['account.account'].search([('id', '=', values['account_id'])])
+            account.value -= values['value']
+        return super(Transaction, self).create(values)
+
+    @api.model
+    def write(self, values):
+        if (values['validated']):
+            account = self.env['account.account'].search([('id', '=', values['account_id'])])
+            account.value -= values['value']
+        return super(Transaction, self).write(values)
 
     @api.onchange('date')
     def _change_date(self):
         transaction_date = fields.Date.from_string(self.date)
         current_date = fields.Date.from_string(fields.Date.today())
         diff_date = current_date - transaction_date
-        if diff_date.months >= 0 or (diff_date.months == 0 and diff_date.days >= 0):
+        if diff_date.days >= 0:
             self.validated = True
         else:
             self.validated = False
