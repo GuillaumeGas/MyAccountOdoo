8f235ea909e53a32b3d47d790a9e7896a81c82b7
GuillaumeGas <guillaume@gas-ntic.fr>
2016-10-02 12:53:31 +0200

    Code de base

---
+++ __openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'MyAccount',
+    'version': '0.1',
+    'category': 'custom',
+    'author': 'Guillaume Gas',
+    'website': 'https://guillaume.gas28.net',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/account.xml',
+        'views/transaction.xml',
+        'views/menu.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'licence': 'GPL',
+}
---
+++ account.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields
+
+
+class Account (models.Model):
+    _name = 'account.account'
+
+    name = fields.Char(string="Compte", required=True)
+    value = fields.Float(string="Solde", default=0.0)
+
+    transaction_ids = fields.One2many('account.transaction', 'account_id', ondelete='cascade')
